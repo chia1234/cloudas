eba93cd45d276bb8af184b224347412a325a8f26
chia1234 <see.you.soon.cwfa@gmail.com>
2015-12-04 22:41:11 +0800

    .

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
- var http = require('http')
-    var port = process.env.PORT || 3000;
-    var host = process.env.HOST || '191.238.181.47';
-    http.createServer(function(req, res) {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('Hello World from Nitrous\n');
-    }).listen(port, host);
-    console.log("Server running at "+host+":"+port);
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
