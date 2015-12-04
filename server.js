5bd4268caa023d4ebb63fe487ba4dbba199588f9
chia1234 <see.you.soon.cwfa@gmail.com>
2015-12-05 02:27:42 +0800

    ,

--- server.js
+++ server.js
@@ -20,4 +20,55 @@ app.get('/', function(req,res) {
 	res.end('Connection closed',200);
 });
 
+// Path 1
+app.get('/name/:name', function(req,res) {
+	console.log('Incoming request: GET');
+	console.log('Request body: ', req.body);
+	console.log('name: ', req.params.name);
+	res.end('Connection closed',200);
+});
+
+// Path 2
+app.get('/age/:age', function(req,res) {
+	console.log('Incoming request: GET');
+	console.log('Request body: ', req.body);
+	console.log('age: ', req.params.age);
+	res.end('Connection closed',200);
+});
+
+// Path 3
+app.get('/name/:name/age/:age', function(req,res) {
+	console.log('Incoming request: GET');
+	console.log('Request body: ', req.body);
+	console.log('name: ', req.params.name);
+	console.log('age: ', req.params.age);
+	res.end('Connection closed',200);
+});
+
+// Path 4
+app.delete('/name/:name', function(req,res) {
+	console.log('Incoming request: DELETE');
+	console.log('Request body: ', req.body);
+	console.log('name: ', req.params.name);
+	res.end('Connection closed',200);
+});
+
+// Path 5
+app.post('/',function(req,res) {
+	console.log('Incoming request: POST');
+	console.log('Request body: ', req.body);
+	console.log('name: ', req.body.name);
+	console.log('age: ', req.body.age);
+	res.end('Connection closed',200);
+});
+
+// Path 6
+app.put('/',function(req,res) {
+	console.log('Incoming request: PUT');
+	console.log('Request body: ', req.body);
+	console.log('name: ', req.body.name);
+	console.log('age: ', req.body.age);
+	res.end('Connection closed',200);
+});
+
 app.listen(process.env.PORT || 1337);
