4885986536e19c179463d82560081a1e4a7b2412
chia1234 <see.you.soon.cwfa@gmail.com>
2015-12-04 22:50:14 +0800

    .

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
+var http = require('http');
+var url  = require('url');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://chia1234:ouhk1234@ds061984.mongolab.com:61984/ouhk');
+//mongoose.connect('mongodb://chia1234:ouhk1234@ds061984.mongolab.com:61984/ouhk');
+var mongodbURL = 'mongodb://chia1234:ouhk1234@ds061984.mongolab.com:61984/ouhk';
+var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
