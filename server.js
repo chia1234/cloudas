6b12841a636fdc435783968d19af7c895bfcce1d
chia1234 <see.you.soon.cwfa@gmail.com>
2015-12-05 00:55:14 +0800

    q

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ var mongoose = require('mongoose');
 mongoose.connect(mongodbURL);
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('heee');
-  res.end('Hello World\n');
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
+	res.end('Hello World\n');
+	if(req.method == "GET"){
+		res.write('Get method\n');
+	}
+	if(req.method == "POST"){
+		res.write("POST method\n");
+	}
 }).listen(port);
