398efe966d33b8a184f81a7d0c42d76e3aa61422
chia1234 <see.you.soon.cwfa@gmail.com>
2015-12-04 21:22:36 +0800

    .

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var mongoose = require('mongoose');
-db = mongoose.connect(uri);
+//db = mongoose.connect(uri);
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
