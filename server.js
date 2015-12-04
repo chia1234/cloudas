a29b40e4c95c826e17f1d7d68af893e372ff5ad4
chia1234 <see.you.soon.cwfa@gmail.com>
2015-12-04 23:31:01 +0800

    ..

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var url  = require('url');
 //mongoose.connect('mongodb://chia1234:ouhk1234@ds061984.mongolab.com:61984/ouhk');
 var mongodbURL = 'mongodb://chia1234:ouhk1234@ds061984.mongolab.com:61984/ouhk';
 var mongoose = require('mongoose');
-console.log('test');
 //var connectionString = process.env.CUSTOMCONNSTR_MONGOLAB_URI;
 //mongoose.connect(connectionString);
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('heee');
   res.end('Hello World\n');
 }).listen(port);
