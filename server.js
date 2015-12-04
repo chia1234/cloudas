3f54c1670e6a567bfae38ffa157bc912089fa3da
chia1234 <see.you.soon.cwfa@gmail.com>
2015-12-04 23:28:02 +0800

    22

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var url  = require('url');
 //mongoose.connect('mongodb://chia1234:ouhk1234@ds061984.mongolab.com:61984/ouhk');
 var mongodbURL = 'mongodb://chia1234:ouhk1234@ds061984.mongolab.com:61984/ouhk';
 var mongoose = require('mongoose');
+console.log('test');
 //var connectionString = process.env.CUSTOMCONNSTR_MONGOLAB_URI;
 //mongoose.connect(connectionString);
 var port = process.env.PORT || 1337;
