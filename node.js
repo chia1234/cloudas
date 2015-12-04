5409545103a4e47f9b748356ba2a6f2dcd87a8ad
chia1234 <see.you.soon.cwfa@gmail.com>
2015-12-04 22:08:56 +0800

    .

--- node.js
+++ node.js
@@ -1,12 +1,6 @@
-var mongoose = require('mongoose');
-db = mongoose.connect(uri);
-//mongoose.connect('mongodb://localhost/myappdatabase');
- //mongodb://<dbuser>:<dbpassword>@ds061984.mongolab.com:61984/ouhk
-
 var http = require('http');
-var url  = require('url');
-
-var server = http.createServer(function (req,res){
-	var greetingMsg = "Hello there!";
-	console.log("INCOMING REQUEST: " + req.connection.remoteAddress + " " + req.method + " " + req.url);
-});
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
