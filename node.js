2703b9182d4109665130fc4297903bd92dd98fe7
chia1234 <see.you.soon.cwfa@gmail.com>
2015-12-04 22:21:18 +0800

    .

--- node.js
+++ node.js
@@ -1,6 +1,21 @@
 var http = require('http');
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+var url = require('url');
+
+function Rectangle(width,length) {
+	this.width = width;
+	this.length = length;
+	this.area = this.width * this.length;
+}
+
+function handle_incoming_request(req, res) {
+	console.log("INCOMING REQUEST: " + req.method + " " + req.url);
+	var parsedURL = url.parse(req.url,true); //true to get query as object 
+	var queryAsObject = parsedURL.query;
+	var obj = new Rectangle(queryAsObject.width, queryAsObject.length);
+
+	res.writeHead(200, {"Content-Type" : "application/json"});
+	res.end(JSON.stringify(obj));
+}
+
+var s = http.createServer(handle_incoming_request);
+s.listen(process.env.PORT || 8099);
