628f79abb5e3ab70cac473a44f923c3ae0d4a61e
chia1234 <see.you.soon.cwfa@gmail.com>
2015-12-05 16:05:11 +0800

    .

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ var http = require('http');
 var url  = require('url');
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+
 //var bodyParser = require('body-parser');
 var RestSchema = require('./models/restaurant');
 console.log("hello");
 
 //app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(bodyParser.json());
-var mongoose = require('mongoose');
 var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
                 replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } },
                  };
@@ -23,27 +24,23 @@ var conn = mongoose.connection;
 conn.on('error', console.error.bind(console, 'connection error:'));
 
 conn.once('open', function() {
-	console.log('connected');
-		var rest = mongoose.model('restaurant', RestSchema);
-		rest.find({},function(err,results){
+var rest = mongoose.model('restaurant', RestSchema);
+console.log('connected');
+app.get('/res/:x', function(req,res){
+	rest.find({},function(err,results){
 		if (err) {
 			console.log("Error");
 			//res.write("Error: " + err.message);
 			//res.json("Error: " + err.message);
 			//db.close();
-			//res.write("Error Message:"+err.message);
-			//res.write("write");
-			//res.end("Not Found json:"+results+"\n",200);
+			// res.write("Error Message:"+err.message);
+			// res.write("write");
+			// res.end("Not Found json:"+results+"\n",200);
 		}
 		else {
-			console.log("FOund"+results);
-				app.get('/res/:x', function(req,res){
-				res.writeHead(200, {'Content-Type': 'text/plain'});
-				res.write('xx');
-				res.json({ message: 'lll'});
-				res.end();
-				console.log(res);
-			});
+			conn.close();
+			console.log("FOund"+results.length);
+			res.json({message:results});
 			//db.close();
 			//res.write('Found: ');
 			
@@ -52,6 +49,8 @@ conn.once('open', function() {
 
 		}
 	});
+});
+		
   // Wait for the database connection to establish, then start the app.                         
 });
-// app.listen(process.env.PORT || 8099);
+app.listen(process.env.PORT || 8099);
