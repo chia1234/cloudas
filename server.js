be0ead9f1599290f4f32eb84787bd83f9bfc2e20
chia1234 <see.you.soon.cwfa@gmail.com>
2015-12-05 13:06:46 +0800

    .

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-var http = require('http');
-var url  = require('url');
+//var http = require('http');
+//var url  = require('url');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var RestSchema = require('./models/restaurant');
+//var RestSchema = require('./models/restaurant');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-var mongoose = require('mongoose');
+//var mongoose = require('mongoose');
 
 //var RestSchema = require('./models/restaurant');
 	// var mongodbURL = 'mongodb://chia1234:ouhk1234@ds061984.mongolab.com:61984/ouhk';
