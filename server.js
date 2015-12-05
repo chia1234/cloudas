8f4abede2ee834740d3cf667c7ab8c9c7ca17b27
chia1234 <see.you.soon.cwfa@gmail.com>
2015-12-05 13:01:31 +0800

    c

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 var mongoose = require('mongoose');
 
-var RestSchema = require('./models/restaurant');
-	var mongodbURL = 'mongodb://chia1234:ouhk1234@ds061984.mongolab.com:61984/ouhk';
-	var mongoose = require('mongoose');
-	mongoose.connect(mongodbURL);
-	var db = mongoose.connection;
-	
+//var RestSchema = require('./models/restaurant');
+	// var mongodbURL = 'mongodb://chia1234:ouhk1234@ds061984.mongolab.com:61984/ouhk';
+	// var mongoose = require('mongoose');
+	// mongoose.connect(mongodbURL);
+	// var db = mongoose.connection;
+
 app.get('/restaurant_id/:x', function(req,res){
 	
 	res.write("x");
