59f866fdb44b9933924a053cd381f5ee9149f34c
chia1234 <see.you.soon.cwfa@gmail.com>
2015-12-05 12:54:19 +0800

    ,

--- server.js
+++ server.js
@@ -6,12 +6,6 @@ var bodyParser = require('body-parser');
 var RestSchema = require('./models/restaurant');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// var MongoClient = require('mongodb').MongoClient;
-//var assert = require('assert');
-// var ObjectId = require('mongodb').ObjectID;
-//mongoose.connect('mongodb://chia1234:ouhk1234@ds061984.mongolab.com:61984/ouhk');
-//var mongodbURL = 'mongodb://chia1234:ouhk1234@ds061984.mongolab.com:61984/ouhk';
 var mongoose = require('mongoose');
 app.get('/restaurant_id/:x', function(req,res){
 	var RestSchema = require('./models/restaurant');
@@ -19,6 +13,8 @@ app.get('/restaurant_id/:x', function(req,res){
 	var mongoose = require('mongoose');
 	mongoose.connect(mongodbURL);
 	var db = mongoose.connection;
+	res.write("x");
+	res.json({ message: 'lll'});
 	//var json = '{}';
 	// var UserSchema = new Schema({
  //    first_name: String,
@@ -30,33 +26,8 @@ app.get('/restaurant_id/:x', function(req,res){
 	//User.find({}, function(err, res){
 		//if (err) res.json({ message: 'lll'});
 	//});
-	res.write("x");
-	res.json({ message: 'lll'});
+	
 });
 
-// app.post('/', function(req,res){
-// 	var RestSchema = require('./models/restaurant');
-// 	var mongodbURL = 'mongodb://chia1234:ouhk1234@ds061984.mongolab.com:61984/ouhk';
-// 	var mongoose = require('mongoose');
-// 	mongoose.connect(mongodbURL);
-// 	var db = mongoose.connection;
-// 	res.write(".:"+mongoose.connection.readyState);
-
-// 	db.on('error', console.error.bind(console, 'connection error:'));
-// 	db.once('open', function (callback) {
-// 		var rest = mongoose.model('restaurant', RestSchema);
-// 		var r = new rest(req.body);
-// 		r.save(function(err,results){
-// 			if (err) {
-// 				res.end("error"+err.message,500);
-// 			}
-// 			else {
-// 				db.close();
-// 				res.end('Done',200);
-// 			}
-// 		});
-// 	});
-// 	db.close();
-// });
 
 app.listen(process.env.PORT || 8099);
